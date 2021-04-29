317607b584eaf7d88e67602aa8b398213e59e1dc
Mani Movassagh <mani.mgh@gmail.com>
2021-04-29 14:37:26 +0200

    after stock

--- src/components/Status.js
+++ src/components/Status.js
@@ -1,13 +1,32 @@
-import React, { Component } from 'react'
 
-// const fetchedData = axios.get('https://api.covid19api.com/summary').then(data=>console.log(data))
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 
-export default class Status extends Component {
-    render() {
-        return (
-            <div>
-         not yet
-            </div>
-        )
-    }
+// 'https://api.covid19api.com/summary'
+//https://jsonplaceholder.typicode.com/posts
+
+
+
+function Status() {
+    const [status, setStatus] = useState([])
+    const [dummi]=useState(1)
+
+    useEffect(() => {
+        console.log('hello world')
+        axios.get('https://api.covid19api.com/summary')
+            .then(res => {
+                setStatus(res.data)
+                console.log(res.data)
+            })
+            .catch(err => console.log(err))
+    }, [dummi])
+
+
+    return (
+        <div>
+            {status.ID}
+        </div>
+    )
 }
+
+export default Status
