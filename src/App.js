c8aea9bae350401b3a0598e886c50ab9f1dba5ed
Mani Movassagh <mani.mgh@gmail.com>
2021-04-29 12:33:04 +0200

    bring data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import Status from './components/Status'
+import React from 'react';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      status: []
+    }
+  }
+
+  componentDidMount() {
+    const statusOfCovid = (data) => {
+      console.log(data);
+      Object.entries(data.Global).forEach(element => {
+          console.log(element);
+
+      });
+      Object.values(data.Countries).forEach(el => {
+          if (el.NewConfirmed > 0) {
+              const div = document.createElement('div');
+              div.innerHTML = el.Country + ` ${el.NewConfirmed} `
+              div.className='text-center p-4  btn-primary'
+              document.body.append(div)
+          }
+      })
+
+  }
+  axios.get('https://api.covid19api.com/summary')
+      .then(function (response) {
+          statusOfCovid(response.data)
+      });
+
+  }
+  render() {
+    return (
+
+      <div>
+       just check
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/Status.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+// const fetchedData = axios.get('https://api.covid19api.com/summary').then(data=>console.log(data))
+
+export default class Status extends Component {
+    render() {
+        return (
+            <div>
+         not yet
+            </div>
+        )
+    }
+}
