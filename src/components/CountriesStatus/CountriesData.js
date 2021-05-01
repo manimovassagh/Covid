e4f146e65c910c1919d3d290c42ec0eafb13cbfc
Mani Movassagh <mani.mgh@gmail.com>
2021-05-01 23:58:41 +0200

    to foreach tomorrow send data inside

--- src/components/CountriesStatus/CountriesData.js
+++ src/components/CountriesStatus/CountriesData.js
@@ -4,10 +4,10 @@ import { DataGrid } from '@material-ui/data-grid';
 
 export default function DataGridDemo(props) {
 
-//Grab fetched Data and Save it to Array to work easier
-    const CountriesData=Object.entries(props)
-    const ArryCountriesdata=CountriesData[0][1]
-    console.log(ArryCountriesdata)
+    //Grab fetched Data and Save it to Array to work easier
+    const CountriesData = Object.entries(props)
+    const ArryCountriesdata = CountriesData[0][1]
+    // console.log(ArryCountriesdata)
     const columns = [
         { field: 'id', headerName: 'ID', width: 70 },
         { field: 'Country', headerName: 'Country', width: 130 },
@@ -17,22 +17,36 @@ export default function DataGridDemo(props) {
         { field: 'TotalConfirmed', headerName: 'TotalC onfirmed', width: 200 },
         { field: 'TotalDeaths', headerName: 'Total Deaths', width: 200 },
         { field: 'TotalRecovered', headerName: 'TotalRecovered', width: 200 },
-        
-       
-      ];
-      
-      const rows = [
-          
-        { id: 1, NewConfirmed: 10, Country: 'Jon', age: 35 },
-      
-      ];
-      
-    
-  return (
+
+    ];
+
+    const dataBank = []
+    let IdCounter = 0
+    for (const countires of ArryCountriesdata) {
+        IdCounter++
+        dataBank.push({ id: IdCounter, Country: countires.Country, NewConfirmed: countires.NewConfirmed })
+
+    }
+    const rows = [
+
+        { id: 1, Country: 'Iran', NewConfirmed: 44, NewDeaths: 49, NewRecovered: 58, TotalConfirmed: 22, TotalDeaths: 36, TotalRecovered: 44, },
+        { id: 2, Country: 'Iran', NewConfirmed: 44, NewDeaths: 49, NewRecovered: 58, TotalConfirmed: 22, TotalDeaths: 36, TotalRecovered: 44, },
+        { id: 3, Country: 'Iran', NewConfirmed: 44, NewDeaths: 49, NewRecovered: 58, TotalConfirmed: 22, TotalDeaths: 36, TotalRecovered: 44, },
+        { id: 4, Country: 'Iran', NewConfirmed: 44, NewDeaths: 49, NewRecovered: 58, TotalConfirmed: 22, TotalDeaths: 36, TotalRecovered: 44, },
+        { id: 5, Country: 'Iran', NewConfirmed: 44, NewDeaths: 49, NewRecovered: 58, TotalConfirmed: 22, TotalDeaths: 36, TotalRecovered: 44, },
+        { id: 6, Country: 'Iran', NewConfirmed: 44, NewDeaths: 49, NewRecovered: 58, TotalConfirmed: 22, TotalDeaths: 36, TotalRecovered: 44, },
+
+    ];
+    console.log(dataBank)
 
 
-    <div style={{ height: 400, width: '100%' }}>
-      <DataGrid rows={rows} columns={columns} pageSize={5}  />
-    </div>
-  );
+
+
+    return (
+
+
+        <div style={{ height: 400, width: '100%' }}>
+            <DataGrid rows={rows} columns={columns} pageSize={5} />
+        </div>
+    );
 }
