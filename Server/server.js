9c5e1b9f829ecf487012861cde1f1209f9b6e6f8
Mani Movassagh <mani.mgh@gmail.com>
2021-05-04 10:39:30 +0200

    add test json file

--- Server/server.js
+++ Server/server.js
@@ -1,18 +1,32 @@
 'use strict';
 const express = require('express');
+const fs = require('fs')
 let cors = require('cors')
 const app = express();
-
 app.use(cors())
 
-app.get('/', (req,res)=>{
-    res.send({hi:'there'})
-    console.log('request made')
-   })
-app.get('/login', (req,res)=>{
-    res.send({user:'tried to login'})
+
+
+//define routers
+app.get('/', (req, res) => {
+    fs.readFile('test.json', (err, data) => {
+        if (err) throw err;
+        else
+        res.send(data.toString())
+        
+    });
+    // console.log('request made')
 })
-app.get('/logout', (req,res)=>{
-    res.send({user:'tried to logout'})
+app.get('/login', (req, res) => {
+    res.send({ user: 'tried to login' })
 })
-app.listen(5000, err => console.log(err || 'Server läuft'));
+app.get('/logout', (req, res) => {
+    res.send({ user: 'tried to logout' })
+})
+app.get('/signup', (req, res) => {
+    res.send({ user: 'Signup from this place' })
+})
+
+// define Ports and heroku settings
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
