5e943e956bdeba7c3c9512338844340cf14aeb22
Mani Movassagh <mani.mgh@gmail.com>
2021-05-05 13:29:26 +0200

    add if to prevent duplicate

--- Server/services/passport.js
+++ Server/services/passport.js
@@ -13,7 +13,14 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-     new User({googleId:profile.id}).save()
+    User.findOne({googleId: profile.id})
+    .then((existingUser)=>{
+      if (existingUser){
+        //we have already a user record with profile ID
+      } else {
+        new User({googleId:profile.id}).save()
+      }
+    })
     }
   )
 );
