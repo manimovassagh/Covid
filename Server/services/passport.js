22c013dbc4c055ed34d68f949d0c416d7add8c43
Mani Movassagh <mani.mgh@gmail.com>
2021-05-05 14:41:49 +0200

    done !!

--- Server/services/passport.js
+++ Server/services/passport.js
@@ -4,6 +4,22 @@ const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
 const User = mongoose.model('users')
+
+
+//serialize and deserialize
+//it is going to bring the unique id from mongodb because 
+//every user have a unique id there and it is not under 
+//influence of google id or facebook or linkdin or anything els and always unique
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
 //use passport js to bring google strategy
 passport.use(
   new GoogleStrategy(
@@ -13,17 +29,17 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id })
-        .then((existingUser) => {
-          if (existingUser) {
-            //we have already a user record with profile ID
-            done(null, existingUser);
-          } else {
-            new User({ googleId: profile.id }).save()
-            .then(user =>done(null,user))
-          }
-        })
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          // we already have a record with the given profile ID
+          done(null, existingUser);
+        } else {
+          // we don't have a user record with this ID, make a new record!
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );
-
