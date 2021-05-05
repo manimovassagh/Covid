58c041dbccb00338964a4238161ee48b924e4754
Mani Movassagh <mani.mgh@gmail.com>
2021-05-05 10:29:12 +0200

    server refactored

--- Server/server.js
+++ Server/server.js
@@ -1,33 +1,13 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+require('./services/passport');
 
+
+//execute server
 const app = express();
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
-    },
-    (accessToken,refreshToken,profile,done) => {
-      console.log('accessToken',accessToken);
-      console.log('refreshToken',refreshToken);
-      console.log('profile',profile);
-    }
-  )
-);
+//grab routers module
+require('./routes/authRoutes')(app);
 
-app.get(
-  '/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email']
-  })
-);
-
-app.get('/auth/google/callback', passport.authenticate('google'));
-
+//define port
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
